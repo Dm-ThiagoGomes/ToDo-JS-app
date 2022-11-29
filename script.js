39a0b1f19671f412d4b66fde96927a2591fe2875
Thiago <dm.thiagogb@hotmail.com>
2022-11-29 14:06:14 -0300

    create the addNote & createNote function (the rescpectives HTML and CSS changes was did

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function addNote () {
-    
+var amountOfNotes = [];
+var divToAddNotes = document.getElementById('notesList');
+var foo = document.getElementsByClassName('note');
+var clickCount = 0;
+
+function addNote() {
+    amountOfNotes.push(amountOfNotes.length + 1);
+
+    var newNote = createNote(`note${amountOfNotes.length + 1}`);
+    divToAddNotes.insertBefore(newNote, divToAddNotes.children[0]);
+}
+
+function createNote(noteIndex) {
+    noteInput = "<input type='text' maxLength = 20>";
+    noteIndex = document.createElement('li');
+    noteIndex.className = 'note';
+    noteIndex.innerHTML += noteInput;
+    return noteIndex;
 }

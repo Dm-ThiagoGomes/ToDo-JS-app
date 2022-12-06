00aadcf5b96c2c031fa8a20233e51a37850b1f19
Thiago <dm.thiagogb@hotmail.com>
2022-12-06 14:14:21 -0300

    Fixed some functions, creating funcion checkNoteId. Fixing getData function.

--- script.js
+++ script.js
@@ -1,113 +1,134 @@
 var ids = [0, 3, 4];
+var amountOfNotes = [];
 var divToAddNotes = document.getElementById('notesList');
 var clickCount = 0;
+var tryTimes = 0;
+var test = [];
+var values = [];
+var dataStorage = []
 
 function addNote(id = 1) {
     var noteId;
     noteId = checkNoteId(id);
     var newNote = createNote(noteId);
     divToAddNotes.insertBefore(newNote, divToAddNotes.children[0]);
-}
-//console.log(amountOfNotes);
 
-function checkNoteId(z) {
-    var test = [0, 1, 2, 3, 4, 5];
-    for (i = 0; i <= ids.length; i++) {
-        if (z == test[i]) {
-            console.log('id used');
-            z++
-            console.log(z);
-            return checkNoteId(z);
-        } else {
-            console.log(typeof(z));
-            console.log(`${z} is an unused id`)
-            return z;
+    //console.log(amountOfNotes);
+
+    function checkNoteId(id) {
+        for (i = 0; i <= test.length; i++) {
+            if (test[i] == id) {
+                console.log('id used');
+                id++
+            } else {
+                console.log(`${id} unused`)
+                test.push(id);
+                test.sort((a, b) => a - b)
+                i = test.length + 1;
+                // console.log(test);
+                return id;
+                //console.log(test[i]);
+            }
         }
+
+    }
+
+    function createNote(noteIndex) {
+        noteInput = "<input type='text' maxLength = 20>";
+        var note = document.createElement('li');
+        note.className = `note`;
+        note.id = noteIndex;
+        deleteNoteButton = `<button onclick= deleteNote(${noteIndex})>X</button>`
+        note.title = noteIndex;
+        note.innerHTML += noteInput;
+        note.innerHTML += deleteNoteButton;
+        return note;
     }
 }
 
-function createNote(noteIndex) {
-    noteInput = "<input type='text' maxLength = 20>";
-    var note = document.createElement('li');
-    note.className = `note`;
-    note.id = noteIndex;
-    deleteNoteButton = `<button onclick= deleteNote(${noteIndex})>X</button>`
-    note.title = noteIndex;
-    note.innerHTML += noteInput;
-    note.innerHTML += deleteNoteButton;
-    return note;
-}
-
-function saveNotes() {
-    var values = [];
-    console.log(values.length)
+async function saveNotes() {
     var note;
     var noteContent;
     var noteTitle;
-    for (i = 1; i <= amountOfNotes.length; i++) {
-        note = document.getElementById(`${i}`);
-        noteContent = note.children.item(0).value;
-        noteTitle = note.getAttribute('title');
-        if (noteContent != '' && noteContent != null) {
-            values.push(`${noteTitle}=${noteContent}`);
+    for (i = 1; i <= test.length; i++) {
+        if (i == test[i - 1]) {
+            note = document.getElementById(`${i}`);
+            noteContent = note.children.item(0).value;
+            noteTitle = note.getAttribute('title');
+            if (noteContent != '') {
+                values.push(`${noteTitle}=${noteContent}`);
+                foo = true
+                console.log('foi com tudo');
+            } else {
+                values.push(`${noteTitle}=`)
+                foo = true
+                console.log('foi com nada');
+            }
         } else {
-            values.push(`${noteTitle}=`)
+            console.log('vai de novo');
         }
     }
+
     if (values.length != 0) {
         for (i in values) {
             var valueToSave = values[i].split('=');
+            console.log(valueToSave);
             localStorage.setItem(valueToSave[0], valueToSave[1]);
             console.log(`${valueToSave[1]} has saved with key ${valueToSave[0]}`);
         }
     } else {
         console.log('nothing to save');
     }
+
+    console.log(localStorage[1]);
 }
 
 function deleteNote(id) {
-    var arrayIndex = amountOfNotes.indexOf(id);
+    var arrayIndex = test.indexOf(id);
     if (arrayIndex > -1) {
-        amountOfNotes.splice(arrayIndex, 1);
+        test.splice(arrayIndex, 1);
     }
-    console.log(amountOfNotes);
     localStorage.removeItem(id);
-    console.log(localStorage.length);
     var note = document.getElementById(`${id}`);
     note.remove();
 }
 
-function getData(i = 0) {
+function getData() {
     var note;
     var storage = localStorage;
-    for (i += 0; i < localStorage.length; i++) {
-        var t = Object.keys(storage)[i];
-        console.log(t);
-        var dataStored = storage.getItem(t);
-        console.log(dataStored);
-        if (dataStored != null) {
-            addNote(t);
-        }
-        note = document.getElementById(`${t}`)
-        if (note == null) {
-            console.log('passou')
-            i++;
-            return getData(i);
-        } else if (note != null) {
-            if (note.id == storage.key(i)) {
-                note.children.item(0).value = storage[note.id];
-            } else {
-                var x = tryAgain(note.id, localStorage.key(i));
-                if (x == note.id) {
-                    note.children.item(0).value = localStorage[i];
-                }
-            }
-            i++
-            return getData(i);
-        } else if (i > localStorage.length) {
-        }
+    //console.log(storage);
 
+    for (i = 0; i < localStorage.length; i++) {
+        if(i == 2)
+        console.log(i);
+        var t = Object.keys(storage);
+        //console.log(t[2])
+        dataStorage.push(parseInt(t[i]));
+        dataStorage.sort((a, b) => a - b);
+        console.log(dataStorage);
     }
+
+    for (i = 1; i <= localStorage.length; i++) {
+        console.log(i);
+        if (i == dataStorage[i - 1]) {
+            console.log(i)
+            var dataStored = storage.getItem(i);
+            console.log(dataStored);
+            addNote(i);
+            // note = document.getElementById(`${i}`)
+            // if (note.id == storage.key(i)) {
+            //     note.children.item(0).value = storage[note.id];
+            // } else {
+            //     var x = tryAgain(note.id, localStorage.key(i));
+            //     if (x == note.id) {
+            //         note.children.item(0).value = localStorage[i];
+            //     }
+            // }
+        } else {
+            console.log('barril aq')
+        }
+    }
+
     function tryAgain(x, y) {
         if (y = x) {
             return y;

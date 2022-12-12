5dc82c2e8eaba5e225a7b0ea740d66d49e383051
Thiago <dm.thiagogb@hotmail.com>
2022-12-12 14:01:16 -0300

    function add note incremented, adding the note in a position by id

--- Controllers/addNote.js
+++ Controllers/addNote.js
@@ -1,14 +1,37 @@
 var divToAddNotes = document.getElementById('notesList');
 
 function addNote(id = 1) {
+    var idsLength = ids.length;
     var noteId;
     noteId = checkNoteId(id);
     var newNote = createNote(noteId);
+    var lastId = ids[idsLength];
+    
     if(noteId == 1){
+        console.log('aq 1');
         var positionBeforeButton = divToAddNotes.children.length-1;
         divToAddNotes.insertBefore(newNote, divToAddNotes.children.item(positionBeforeButton));
-    }else{
-        divToAddNotes.insertBefore(newNote, divToAddNotes.children[0]);
+    }else if (noteId < lastId)
+    {
+        console.log('aq 2');
+        addIndex = noteId-1
+        var foo =divToAddNotes.children.namedItem(`${addIndex}`)
+        console.log(foo);
+        divToAddNotes.insertBefore(newNote, foo);
+    } else {
+        console.log('aq 3');
+        divToAddNotes.insertBefore(newNote, divToAddNotes.children.item(0));
+    }
+    function createNote(noteIndex) {
+        noteInput = "<input type='text' maxLength = 20>";
+        var note = document.createElement('li');
+        note.className = `note`;
+        note.id = noteIndex;
+        deleteNoteButton = `<button onclick= deleteNote(${noteIndex})>X</button>`
+        note.title = noteIndex;
+        note.innerHTML += noteInput;
+        note.innerHTML += deleteNoteButton;
+        return note;
     }
 
     function checkNoteId(id) {
@@ -29,16 +52,5 @@ function addNote(id = 1) {
 
     }
 
-    function createNote(noteIndex) {
-        noteInput = "<input type='text' maxLength = 20>";
-        var note = document.createElement('li');
-        note.className = `note`;
-        note.id = noteIndex;
-        deleteNoteButton = `<button onclick= deleteNote(${noteIndex})>X</button>`
-        note.title = noteIndex;
-        note.innerHTML += noteInput;
-        note.innerHTML += deleteNoteButton;
-        return note;
-    }
 
 }
